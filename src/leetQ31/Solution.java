7059ea77bb5054f45e53627185d8133c7b3c408f
lowBiCoder <1768815224@qq.com>
2018-12-23 13:48:14 +0800

    leetQ31 copy

--- src/leetQ31/Solution.java
+++ src/leetQ31/Solution.java
@@ -4,4 +4,56 @@ package leetQ31;
  * Created by zhangzhongwen on 2018/12/23
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 2};
+        nextPermutation(nums);
+        for (int i :
+                nums) {
+            System.out.println(i);
+        }
+    }
+
+    public static void nextPermutation(int[] nums) {
+        if(nums.length<=1){
+            return;
+        }
+
+        int i= nums.length-1;
+
+        for(;i>=1;i--){
+
+            if(nums[i]>nums[i-1]){ //find first number which is smaller than it's after number
+                break;
+            }
+        }
+
+        if(i!=0){
+            swap(nums,i-1); //if the number exist,which means that the nums not like{5,4,3,2,1}
+        }
+
+        reverse(nums,i);
+    }
+
+    private static void swap(int[] a,int i){
+        for(int j = a.length-1;j>i;j--){
+            if(a[j]>a[i]){
+                int t = a[j];
+                a[j] = a[i];
+                a[i] = t;
+                break;
+            }
+        }
+    }
+
+    private static void reverse(int[] a,int i){//reverse the number after the number we have found
+        int first = i;
+        int last = a.length-1;
+        while(first<last){
+            int t = a[first];
+            a[first] = a[last];
+            a[last] = t;
+            first ++;
+            last --;
+        }
+    }
 }
