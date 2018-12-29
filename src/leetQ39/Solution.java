32ef45bd084f77ab563656d5c76f46e4b6700e12
lowBiCoder <1768815224@qq.com>
2018-12-29 17:19:31 +0800

    leetQ39  copy

---
+++ src/leetQ39/Solution.java
@@ -0,0 +1,35 @@
+package leetQ39;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zhangzhongwen on 2018/12/29
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(combinationSum(new int[]{2, 3, 6, 7}, 7));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        getResult(result, new ArrayList<Integer>(), candidates, target, 0);
+
+        return result;
+    }
+
+    private static void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start) {
+        if (target > 0) {
+            for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
+                cur.add(candidates[i]);
+                getResult(result, cur, candidates, target - candidates[i], i);
+                cur.remove(cur.size() - 1);
+            }//for
+        }//if
+        else if (target == 0) {
+            result.add(new ArrayList<Integer>(cur));
+        }//else if
+    }
+}
