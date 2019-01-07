f83b5e09bea29f7616cfae08f12a73b13ddaf09c
lowBiCoder <1768815224@qq.com>
2019-01-07 10:38:08 +0800

    leetQ48  copy

--- src/leetQ48/Solution.java
+++ src/leetQ48/Solution.java
@@ -20,29 +20,17 @@ public class Solution {
 
     public static void rotate(int[][] matrix) {
         //method one
-//         if ( matrix == null || matrix[0] == null || matrix[0].length == 0) {
-//            return;
-//        }
-//        int top = 0;
-//        int left = 0;
-//        int bottom = matrix.length - 1;
-//        int right = matrix.length - 1;
-//        int n = matrix.length;
-//        while( n > 1 ) {
-//            for ( int i = 0; i < n - 1; i++ ) {
-//                int temp = matrix[top][left + i];
-//                matrix[top][left + i] = matrix[bottom - i][left];
-//                matrix[bottom - i][left] = matrix[bottom][right - i];
-//                matrix[bottom][right - i] = matrix[top + i][right];
-//                matrix[top + i][right] = temp;
+//        int[][] ans = new int[matrix.length][matrix.length];
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                int index = matrix.length - 1 - i;
+//                System.out.println("[" + i + "][" + j + "]==[" + j + "][" + index + "]");
+//                System.out.println(matrix[i][j] + "==" + matrix[j][index]);
+//                ans[j][index] = matrix[i][j];
 //            }
-//
-//            top++;
-//            left++;
-//            right--;
-//            bottom--;
-//            n -= 2;
 //        }
+//
+//        System.arraycopy(ans, 0, matrix, 0, ans.length);
 
         // method two
         if (matrix == null || matrix[0] == null || matrix[0].length == 0) {
