e6b96343c5bd85662c9f1361ecf3d87d89263329
lowBiCoder <1768815224@qq.com>
2019-01-07 11:38:57 +0800

    leetQ48  copy

--- src/leetQ49/Solution.java
+++ src/leetQ49/Solution.java
@@ -1,7 +1,27 @@
 package leetQ49;
 
+import java.util.*;
+
 /**
  * Created by zhangzhongwen on 2019/1/7
  */
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<List<String>>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<String>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+
 }
