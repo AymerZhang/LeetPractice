53e6d2156bcac49d1c5bc668782d53367cef021c
lowBiCoder <1768815224@qq.com>
2019-01-09 10:43:43 +0800

    leetQ53 copy

--- src/leetQ53/Solution.java
+++ src/leetQ53/Solution.java
@@ -4,4 +4,16 @@ package leetQ53;
  * Created by zhangzhongwen on 2019/1/9
  */
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
 }
--- src/leetQ53/Solution.kt
+++ src/leetQ53/Solution.kt
@@ -3,3 +3,19 @@ package leetQ53
 /**
  * Created by zhangzhongwen on 2019/1/9
  */
+
+fun main(args: Array<String>) {
+    print(maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
+}
+
+fun maxSubArray(nums: IntArray): Int {
+    var maxSoFar: Int = nums[0]
+    var maxEndingHere: Int = nums[0]
+    nums.indices.forEach { i ->
+        if (i != 0) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i])
+            maxSoFar = Math.max(maxSoFar, maxEndingHere)
+        }
+    }
+    return maxSoFar
+}
