fba8c55b48088927ef7877b46ca151ed520b1bdb
lowBiCoder <1768815224@qq.com>
2019-01-22 14:50:39 +0800

    leetQ67 copy

--- src/leetQ67/Solution.java
+++ src/leetQ67/Solution.java
@@ -4,4 +4,41 @@ package leetQ67;
  * Created by zhangzhongwen on 2019/1/22
  */
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(addBinary("11", "1"));
+        System.out.println(addBinary("1010", "1011"));
+    }
+
+
+    public static String addBinary(String a, String b) {
+        int carry = 0;
+        int sum = 0;
+        int opa = 0;
+        int opb = 0;
+        StringBuilder result = new StringBuilder();
+        while (a.length() != b.length()) {
+            if (a.length() > b.length()) {
+                b = "0" + b;
+            } else {
+                a = "0" + a;
+            }
+        }
+        for (int i = a.length() - 1; i >= 0; i--) {
+            opa = a.charAt(i) - '0';
+            opb = b.charAt(i) - '0';
+            sum = opa + opb + carry;
+            if (sum >= 2) {
+                result.append((char) (sum - 2 + '0'));
+                carry = 1;
+            } else {
+                result.append((char) (sum + '0'));
+                carry = 0;
+            }
+        }
+        if (carry == 1) {
+            result.append("1");
+        }
+        return result.reverse().toString();
+    }
+
 }
